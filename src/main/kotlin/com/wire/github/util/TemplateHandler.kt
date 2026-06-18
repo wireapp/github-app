41a59cb31464514c1f3ca483c848af1b1e35f6b7
SimonThormeyer <simon.thormeyer@wire.com>
2026-06-18 14:21:24 +0200

    chore: update template handler

--- src/main/kotlin/com/wire/github/util/TemplateHandler.kt
+++ src/main/kotlin/com/wire/github/util/TemplateHandler.kt
@@ -16,11 +16,15 @@ class TemplateHandler {
     fun handleEvent(
         event: String,
         response: GitHubResponse
-    ): String? =
-        try {
+    ): String? {
+        val path = templatePath(
+            event = event,
+            response = response
+        ) ?: return null
+
+        return try {
             val template = compileTemplate(
-                event = event,
-                action = response.action
+                path = path
             )
 
             populateTemplate(
@@ -31,28 +35,54 @@ class TemplateHandler {
             logger.error("MustacheNotFoundException: $exception")
             null
         }
+    }
 
-    private fun compileTemplate(
+    private fun templatePath(
         event: String,
-        action: String?
-    ): Mustache {
-        val path = action?.let {
-            String.format(
-                Locale.getDefault(),
-                "$TEMPLATE_DIRECTORY/%s/%s.%s.template",
-                LANGUAGE_ENGLISH,
-                event,
-                it
-            )
-        } ?: String.format(
+        response: GitHubResponse
+    ): String? =
+        when (event) {
+            EVENT_CHECK_SUITE ->
+                response.checkSuite
+                    ?.takeIf { it.successful || it.failed }
+                    ?.let { eventTemplatePath(event) }
+            EVENT_STATUS ->
+                response
+                    .takeIf { it.statusSuccessful || it.statusFailed }
+                    ?.let { eventTemplatePath(event) }
+            EVENT_WORKFLOW_RUN ->
+                response.workflowRun
+                    ?.takeIf { it.successful || it.failed }
+                    ?.let { eventTemplatePath(event) }
+            else -> response.action?.let {
+                actionTemplatePath(
+                    event = event,
+                    action = it
+                )
+            } ?: eventTemplatePath(event)
+        }
+
+    private fun actionTemplatePath(
+        event: String,
+        action: String
+    ): String =
+        String.format(
+            Locale.getDefault(),
+            "$TEMPLATE_DIRECTORY/%s/%s.%s.template",
+            LANGUAGE_ENGLISH,
+            event,
+            action
+        )
+
+    private fun eventTemplatePath(event: String): String =
+        String.format(
             Locale.getDefault(),
             "$TEMPLATE_DIRECTORY/%s/%s.template",
             LANGUAGE_ENGLISH,
             event
         )
 
-        return mustacheFactory.compile(path)
-    }
+    private fun compileTemplate(path: String): Mustache = mustacheFactory.compile(path)
 
     private fun populateTemplate(
         mustache: Mustache,
@@ -66,5 +96,8 @@ class TemplateHandler {
     private companion object {
         const val LANGUAGE_ENGLISH = "en"
         const val TEMPLATE_DIRECTORY = "templates"
+        const val EVENT_CHECK_SUITE = "check_suite"
+        const val EVENT_STATUS = "status"
+        const val EVENT_WORKFLOW_RUN = "workflow_run"
     }
 }
