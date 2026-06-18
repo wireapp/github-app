4f3fbf647157013801bd66fc76c2b34129d3e782
SimonThormeyer <simon.thormeyer@wire.com>
2026-06-18 15:44:45 +0200

    feat: update `WorkflowRun` model

--- src/main/kotlin/com/wire/github/response/model/WorkflowRun.kt
+++ src/main/kotlin/com/wire/github/response/model/WorkflowRun.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class WorkflowRun(
+    val id: Long? = null,
     val name: String? = null,
     @SerialName("html_url")
     val htmlUrl: String? = null,
@@ -20,9 +21,23 @@ data class WorkflowRun(
 
     val failed: Boolean
         get() = conclusion in WORKFLOW_RUN_FAILURE_CONCLUSIONS
+
+    val completed: Boolean
+        get() = status == WORKFLOW_RUN_STATUS_COMPLETED || conclusion != null
+
+    val inProgress: Boolean
+        get() = !completed
+
+    val cancelled: Boolean
+        get() = conclusion == WORKFLOW_RUN_CONCLUSION_CANCELLED
+
+    val neutral: Boolean
+        get() = completed && !successful && !failed && !cancelled
 }
 
 private const val WORKFLOW_RUN_CONCLUSION_SUCCESS = "success"
+private const val WORKFLOW_RUN_CONCLUSION_CANCELLED = "cancelled"
+private const val WORKFLOW_RUN_STATUS_COMPLETED = "completed"
 
 private val WORKFLOW_RUN_FAILURE_CONCLUSIONS = setOf(
     "failure",
