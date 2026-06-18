a2a8f5e9b06720c52c1f960c011a3ecf958b4fcb
SimonThormeyer <simon.thormeyer@wire.com>
2026-06-18 14:21:29 +0200

    test: update tests

--- src/test/kotlin/com/wire/github/ApplicationTest.kt
+++ src/test/kotlin/com/wire/github/ApplicationTest.kt
@@ -8,6 +8,7 @@ import com.wire.sdk.model.QualifiedId
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
@@ -69,7 +70,11 @@ class ApplicationTest {
             }
             val response = client.get("/health")
 
-            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                HttpStatusCode.OK,
+                response.status,
+                response.bodyAsText()
+            )
         }
 
     @Test
