8127803aeacc1f76fae6959c1212ff2b786ab76a
Alexandre Ferris <ferris.alexandre@gmail.com>
2026-07-06 17:20:52 +0200

    chore: Add prefix for Redis keys (#120)
    * add prefix namespace to be used in keys

--- src/main/kotlin/com/wire/github/util/Extensions.kt
+++ src/main/kotlin/com/wire/github/util/Extensions.kt
@@ -2,6 +2,8 @@ package com.wire.github.util
 
 import com.wire.sdk.model.QualifiedId
 
-fun QualifiedId.toStorageKey() = "${this.id}@${this.domain}"
+private const val STORAGE_KEY_PREFIX = "github-app:"
 
-fun String.toStorageKey(domain: String) = "$this@$domain"
+fun QualifiedId.toStorageKey() = "$STORAGE_KEY_PREFIX${this.id}@${this.domain}"
+
+fun String.toStorageKey(domain: String) = "$STORAGE_KEY_PREFIX$this@$domain"
